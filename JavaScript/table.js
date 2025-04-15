0ad95ae0650b637cd6d3417910711e39b22b5ad0
Mihail <korikov.me@students.dvfu.ru>
2025-04-15 15:23:37 +1000

    Добавил стили для таблицы и сделал её отображение

---
+++ JavaScript/table.js
@@ -0,0 +1,25 @@
+
+//вывод таблицы на страницу
+let createTable = (data, idTable) => {
+
+    let table = document.getElementById(idTable);
+    
+    let tr = document.createElement('tr');
+
+    for(key in data[0]) {
+        let th = document.createElement('th');
+        th.innerHTML = key;
+        tr.append(th);
+    }
+    table.append(tr);
+
+    data.forEach((item, ind) => {
+        let tr = document.createElement('tr');
+        for (let key in item) {
+            let th = document.createElement('td');
+            th.innerHTML = item[key];
+            tr.append(th);
+        }
+        table.append(tr);
+    });
+}
